e8c14bc1174c08d5eb1bd7f268325c8b14f45f5a
Alex <aleksreut@yandex.ru>
2021-12-04 15:27:30 +0500

    first commit

---
+++ as.js
@@ -0,0 +1,11 @@
+ 'use strict';
+
+// const category = 'toys';
+
+// console.log(`https://mysite.ru/${category}/5`);
+
+// const user = 'Иван';
+
+// alert(`Привет ${user}`);
+console.log(2*4 === 8);
+console.log(2*4 === '8');
